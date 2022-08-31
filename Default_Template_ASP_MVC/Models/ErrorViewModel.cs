d7f726a11d3095768532f57c9da6159b2fa0c57b
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-31 03:06:44 +0200

    leggo

---
+++ Default_Template_ASP_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Default_Template_ASP_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
